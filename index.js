f5135d48ab1ae98e65dca183c8830656d50c0bfc
TiagoSSCarneiro <tiagosergio23@gmail.com>
2022-04-06 21:02:12 -0300

    introduzindo input

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-
-function carregar() {
+function horas() {
     let msg = document.getElementById('msg')
     let img = document.getElementById('imagem')
-    let data = new Date()
+    let horaInformada = document.getElementById('txthr')
+    let hora = horaInformada.value 
+    console.log(hora)
+    //let data = new Date()
     //let hora = data.getHours()
-    let hora = prompt('Informe que horas são: ')
-    
     if (hora >= 0 && hora < 12) {
         //bom dia
         img.src = 'manha.png'
         msg.innerHTML = 'Bom dia!'
         document.body.style.background = '#e0dfaf'
-        
-    }else if(hora >= 12 && hora <= 18) {
+
+    } else if (hora >= 12 && hora <= 18) {
         img.src = 'entardecer.png'
         msg.innerHTML = 'Boa tarde!'
         document.body.style.background = '#eea36e'
-        
+
         //boa tarde
-    }else{
+    } else {
         img.src = 'noite.png'
         msg.innerHTML = 'Boa noite!'
         document.body.style.background = '#0f3043'
